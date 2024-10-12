d7413b7281ed16ccef7c2904f5887e445b52f612
wajahat414 <wajahatakram414@gmail.com>
2024-10-12 11:28:18 +0400

    Initial Commit

---
+++ ios18BottomNavigationBug/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ios18BottomNavigationBug
+//
+//  Created by devloper on 10/12/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ios18BottomNavigationBug/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ios18BottomNavigationBug
+//
+//  Created by devloper on 10/12/24.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
